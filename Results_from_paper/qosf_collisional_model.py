711027b9774341e4e42f464691961efbc9a934c7
Newtech66 <78967475+Newtech66@users.noreply.github.com>
2023-02-11 19:04:46 +0530

    Update qosf_collisional_model.py

--- Results_from_paper/qosf_collisional_model.py
+++ Results_from_paper/qosf_collisional_model.py
@@ -2,14 +2,14 @@ import cirq
 import numpy as np
 import matplotlib.pyplot as plt
 
-def prepare_correlated(env):
+def prepare_correlated(env,n=2):
     yield cirq.H(env[0])
     yield cirq.CNOT(env[0],env[1])
     yield cirq.CNOT(env[1],env[2])
 
-def prepare_uncorrelated(env):
-    for qb in env:
-        yield cirq.H(qb)
+def prepare_uncorrelated(env,n):
+    for i in range(n):
+        yield cirq.H(env[i])
 
 def collide(env,sys,theta):
     yield cirq.CNOT(env,sys)
@@ -30,7 +30,7 @@ def meas_coherence(sys):
 
 def collision_circuit(env,sys,prepare_ancilla,collision_pattern,n,theta):
     yield cirq.H(sys)
-    yield from prepare_ancilla(env)
+    yield from prepare_ancilla(env,n)
     yield from collision_pattern(env,sys,n,theta)
     yield from meas_coherence(sys)
 
@@ -53,20 +53,19 @@ for i in range(1,n+1):
     circuit = cirq.Circuit(collision_circuit(env,sys,prepare_correlated,collision_pattern_correlated,i,theta))
     result = simulator.run(circuit,repetitions=repcnt)
     histogram = result.histogram(key = 'answer')
-
-    rho_12_corr.append((histogram[0]-histogram[1])/repcnt)
+    rho_12_corr.append((histogram[0]-histogram[1])/(2*repcnt))
 
 for i in range(1,n+1):
     circuit = cirq.Circuit(collision_circuit(env,sys,prepare_uncorrelated,collision_pattern_uncorrelated,i,theta))
     result = simulator.run(circuit,repetitions=repcnt)
     histogram = result.histogram(key = 'answer')
-    rho_12_uncorr.append((histogram[0]-histogram[1])/repcnt)
+    rho_12_uncorr.append((histogram[0]-histogram[1])/(2*repcnt))
 
 def corrfunc(time):
-    return np.cos(time)**2-np.sin(time)**2
+    return (np.cos(time)**2-np.sin(time)**2)/2
 
 def uncorrfunc(i):
-    return np.power(np.cos(2*g*tau),i/tau)
+    return np.power(np.cos(2*g*tau),i/tau)/2
 
 x_ax = np.linspace(0,t[-1],num=1000)
 
